638b1397472374a0778602e0cc67e170601207ed
Fernandoyama10 <74337206+Fernandoyama10@users.noreply.github.com>
2021-03-16 16:35:22 -0600

    Proyectito 3
    Version nueva

---
+++ Proyectito3/controllers/usuariosController.js
@@ -0,0 +1,100 @@
+//conexion a la base de datos
+var conexion = require('../config/conexion');
+
+//obtener los metodos de sql del modelo usuario
+var usuario= require('../model/usuario');
+
+//libreria filesystem que permite borrar la imagen
+var borrar= require("fs");
+module.exports={
+
+    //obtener datos en el index de /users para mandar los datos a la vista users
+index:function(req,res){
+usuario.getdata(conexion,function (err,datos) {
+    console.log(datos);
+    //variable usuarios:datos recibe los datos de la consulta.
+    res.render('users/index', { title: 'Usuarios', usuarios:datos });
+});
+},
+
+//crear ruta para formulario crear usuario
+newuser:function (req,res) {
+    res.render('users/newuser');
+},
+
+//funcion para guardar usuario en la base de datos y guardar imagen
+save:function (req,res) {
+  console.log(req.body);
+  console.log(req.file.filename);
+  // req.body son los datos recibidos por el formulario y req.file es el archivo imagen
+usuario.insert(conexion,req.body,req.file,function (err,datos) {
+   res.redirect('/users');
+});
+},
+
+//funcion que borra el usuario y la imagen correspondiente
+delete:function (req,res){
+  console.log("Recepción de datos");
+  console.log(req.params.id);
+  //retorna el nombre de la imagen para poder localizarla y borrarla
+  usuario.returnPerId(conexion,req.params.id, function(err,registros){
+    var nombreImagen="public/images/"+(registros[0].image);
+   
+  //condicional para verificar si existe la imagen en la carpeta public/images, si es asi lo borra
+    if(borrar.existsSync(nombreImagen)){
+      borrar.unlinkSync(nombreImagen);
+    }
+    //funcion que permite borrar por el id del usuario (se obtiene por req.params.id) 
+    usuario.deletePerId(conexion,req.params.id,function(err) {
+      res.redirect('/users');
+    });
+   
+
+  });
+},
+//funcion para recibir los datos del usuario mediante su id para llenarlos en el formulario de actualizar datos usuario
+edit:function (req,res){
+  usuario.returnPerId(conexion,req.params.id, function(err,registros){
+    console.log(registros[0]);
+    res.render('users/edit', {user:registros[0]});
+  });
+ 
+},
+
+//actualiza datos del usuario y la imagen la reemplazada en dado caso que meta una nueva el administrador
+update:function (req,res) {
+  console.log(req.body.name);
+
+  if(req.file){
+    if(req.file.filename){
+
+  //retorna el nombre de la imagen para poder localizarla y borrarla
+  usuario.returnPerId(conexion,req.body.id_user, function(err,registros){
+    var nombreImagen="public/images/"+(registros[0].image);
+  //condicional para verificar si existe la imagen en la carpeta public/images, si es asi lo borra
+    if(borrar.existsSync(nombreImagen)){
+      borrar.unlinkSync(nombreImagen);
+    }
+    //actualizar imagen en la bd
+    usuario.updateFileUser(conexion,req.body,req.file,function (err){
+    });
+
+  });
+
+   
+
+    }
+  }
+//condiciona los textboxs y ejecuta la actualizacion de los datos (sin la imagen)
+  if(req.body.name || req.body.password || req.body.email){
+    usuario.updateUser(conexion,req.body,function (err){
+    });
+  }
+  
+
+  res.redirect('/users');
+ 
+ 
+}
+
+}
---
+++ Proyectito3/model/usuario.js
@@ -0,0 +1,20 @@
+module.exports={
+    getdata:function (conexion,funcion){
+        conexion.query("SELECT * FROM usuarios", funcion);
+    },
+    insert:function (conexion,datos,archivos,funcion){
+        conexion.query("INSERT INTO usuarios ( name, image, password, email) VALUES (?,?,?,?) ",[datos.name, archivos.filename, datos.password, datos.email], funcion);
+    },
+    returnPerId:function (conexion,id,funcion){
+        conexion.query("SELECT * FROM usuarios WHERE id_user=?",[id], funcion);
+    },
+    deletePerId:function (conexion,id,funcion){
+        conexion.query("DELETE FROM usuarios WHERE id_user=?",[id], funcion);
+    },
+    updateUser:function (conexion,datos,funcion){
+        conexion.query("UPDATE usuarios SET name=?, password=?, email=? WHERE id_user=? ",[datos.name, datos.password, datos.email, datos.id_user],funcion);
+    },
+    updateFileUser:function (conexion,datos,archivo,funcion){
+        conexion.query("UPDATE usuarios SET image=? WHERE id_user=? ",[archivo.filename, datos.id_user],funcion);
+    },
+}
