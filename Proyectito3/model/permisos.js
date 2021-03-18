1b9eef5cbf7699d21468f16b00e111649fd0213b
beds10 <73314223+beds10@users.noreply.github.com>
2021-03-18 17:44:16 -0600

    ACT

--- Proyectito3/model/permisos.js
+++ Proyectito3/model/permisos.js
@@ -2,4 +2,7 @@ module.exports={
     getRoles:function (conexion,id,funcion){
         conexion.query('SELECT id_operation, status, id_module FROM operations where id_user=?',[id], funcion);
     },
+    getModulesPerName:function (conexion,id,namepage,funcion){
+        conexion.query('SELECT id_operation, status, O.id_module, name_module FROM operations as O INNER JOIN module ON O.id_module = module.id_module where id_user=? and name_module=?',[id,namepage], funcion);
+    },
 }
